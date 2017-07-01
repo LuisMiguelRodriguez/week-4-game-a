cfae30c43d194339550c91a4a26761422a3d2451
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-07-01 09:01:04 -0700

    created random number function for points to match, also added html and css to show points

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
   var game1 = new Crystal();
 
   game1.addValues();
+  console.log(game1.pointsToMatch());
 
   $('#red').on("click", function() {
 
@@ -27,15 +28,20 @@
 
 
   function Crystal (){
+    //function to pick a number from 12 - 120
+    this.pointsToMatch = function(){
+      return Math.floor((Math.random(12) * ( 120 - 12) + 12));
+    };
+
     this.crystals =  ["red","blue","black","green"];
     this.numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
     this.counter = 0;
+    this.points = 0;
     this.randomNumber = function (){
       return this.numbers[Math.floor((Math.random() * this.numbers.length))];
     };
 
     this.addValues = function(){
-
       for (var i = 0; i < this.crystals.length; i++){
         var value = this.randomNumber();
         console.log(value);
@@ -46,6 +52,9 @@
         this.numbers.splice(index , 1);
         console.log(this.numbers);
       }
+
+      $("#pointsToMatch").html(this.pointsToMatch());
+      $("#points").html(this.points);
     };
 
   }
